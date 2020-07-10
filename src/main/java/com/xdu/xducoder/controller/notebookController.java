3de5222017a9b7ca13a52fabfaac216abeaac31f
Ordi_P <63042018+Ordi-P@users.noreply.github.com>
2020-07-10 12:32:24 +0800

    细节修正与完善

--- src/main/java/com/xdu/xducoder/controller/notebookController.java
+++ src/main/java/com/xdu/xducoder/controller/notebookController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(allowCredentials="true",origins = "*",maxAge = 3600)
 @RestController
 public class notebookController {
     @Autowired
